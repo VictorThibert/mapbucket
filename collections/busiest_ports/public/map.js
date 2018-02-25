36ca20c94fec5b6ef3e5cd2517e6a4a890089a88
Victor Thibert <victor.thibert@gmail.com>
2018-02-25 02:10:58 -0500

    updated for d3 v4

--- collections/busiest_ports/public/map.js
+++ collections/busiest_ports/public/map.js
@@ -1,58 +1,46 @@
-function zoomed() {
-  features.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-  features.select(".boundary")
-    .style("stroke-width", Math.pow(0.5 / d3.event.scale, 0.7) + "px");
-  features.selectAll(".marker")  
-    .style("stroke-width", Math.pow(0.5 / d3.event.scale, 0.7) + "px");
-  features.selectAll("circle")
-    .attr("r", function(d){
-        return Math.sqrt(d['teu']) / 15 / Math.pow(d3.event.scale, 0.5)
-      }) // exponent to slowly make circles bigger
-}
+let colorScale = d3.scaleLinear().domain([0, 100]).range(["#ea765d", "#6e8fb7"]);
+// set dimensions of graphic
+let width = 960;
+let height = 550;
+let radiusDivider = 12
 
-let colorScale = d3.scale.linear().domain([0, 100]).range(["#ea765d", "#6e8fb7"]);
-
-
-let zoom = d3.behavior.zoom()
-  .translate([0,0])
-  .scale(1)
-  .scaleExtent([1,8])
+// create zoom object
+let zoom = d3.zoom()
+  .translateExtent([[0, 0], [width, height]])
+  .scaleExtent([1, 8])
   .on("zoom", zoomed);
 
-let width = 960,
-    height = 550;
-
 // define projection and viewing bounds
-let projection = d3.geo.naturalEarth()
-    .scale(200)
-    .translate([width / 2, height / 2 + 50]) // +50 for antarctica removal
-    .precision(.1);
+let projection = d3.geoNaturalEarth()
+  .scale(200)
+  .translate([width / 2, height / 2 + 50]) // +50 for antarctica removal
+  .precision(.1);
 
-let path = d3.geo.path()
-    .projection(projection);
+// create path from projection
+let path = d3.geoPath()
+  .projection(projection);
 
-let graticule = d3.geo.graticule();
+let graticule = d3.geoGraticule();
 
 // create svg to house map
 let svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .call(zoom)
-
+  .attr("width", width)
+  .attr("height", height)
+  .call(zoom)
+    
 // create features to house elements of the map
 let features = svg.append("g");   
 
 // create tooltip div (invisible)
 let tooltip = d3.select("body").append("div") 
-    .attr("class", "tooltip")       
-    .style("opacity", 0);
+  .attr("class", "tooltip")       
+  .style("opacity", 0);
 
 // read in world coordinates
 d3.json("../assets/world_minus_antarctica.json", function(error, world) {
   if (error) throw error;
 
   let countries = topojson.feature(world, world.objects.countries).features;
-      
 
   features.selectAll(".country")
       .data(countries)
@@ -74,7 +62,7 @@ d3.json("../assets/world_minus_antarctica.json", function(error, world) {
       .attr("class", "marker")
       .attr("cx", function(d){return projection([d['lon'],d['lat']])[0] })
       .attr("cy",function(d){return projection([d['lon'],d['lat']])[1] })
-      .attr("r", function(d){return Math.sqrt(d['teu'])/15 }) //consider proportional to square root
+      .attr("r", function(d){return Math.sqrt(d['teu']) / radiusDivider }) //consider proportional to square root
       .style("fill", function(d){return colorScale(d['rank'])})
       .on("mouseover", function(d) {  
         tooltip.transition()    
@@ -93,3 +81,17 @@ d3.json("../assets/world_minus_antarctica.json", function(error, world) {
 });
 
 d3.select(self.frameElement).style("height", height + "px");
+
+// adjust graphics on zoom
+function zoomed() {
+  features.attr("transform", d3.event.transform); // updated for d3 v
+  features.select(".boundary")
+    .style("stroke-width", Math.pow(0.5 / d3.event.transform.k, 0.7) + "px");
+  features.selectAll(".marker")  
+    .style("stroke-width", Math.pow(0.5 / d3.event.transform.k, 0.7) + "px");
+  features.selectAll("circle")
+    .attr("r", function(d){
+        // exponent to slowly make circles bigger
+        return Math.sqrt(d['teu']) / radiusDivider / Math.pow(d3.event.transform.k, 0.5)
+      }) 
+}
